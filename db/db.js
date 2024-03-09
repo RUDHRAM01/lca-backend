d8ac06db8667a62fa2d0f92c668af6b7e89afd3a
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2024-03-09 23:52:33 +0530

    Update database connection URL

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const db = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/lca');
+        await mongoose.connect(process.env.DB_URL);
         console.log('Connected to database');
     } catch (error) {
         console.error('Error connecting to database: ', error);
