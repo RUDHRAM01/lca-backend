dbf276a9a663ba10bf0b488f899d7cd9a98748fb
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2024-03-09 23:52:13 +0530

    Add User and Project routes, database connection, and authentication middleware

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 4000;
-
+const UserRouter = require('./routes/UserRoute');
+const ProjectRouter = require('./routes/ProjectRoute');
+const db = require('./db/db');
+const Auth = require('./middleware/Auth');
+db();
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use('/api/user', UserRouter);
+app.use('/api/project',[Auth], ProjectRouter);
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
