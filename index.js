2e5fb662a751c9c1c098771d4add480c5079288f
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2024-03-10 16:57:57 +0530

    Refactor code: Update import statements and add MainRouter

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 4000;
-const UserRouter = require('./routes/UserRoute');
-const ProjectRouter = require('./routes/ProjectRoute');
-const db = require('./db/db');
-const Auth = require('./middleware/Auth');
+const UserRouter = require("./routes/UserRoute");
+const ProjectRouter = require("./routes/ProjectRoute");
+const MainRouter = require("./routes/Main");
+const db = require("./db/db");
+const callingFunction = require("./routes/Main");
+const Auth = require("./middleware/Auth");
 db();
 
+
 const corsOptions = {
-    origin: 'http://localhost:3000',
-    credentials: true 
-  };
+  origin: "http://localhost:3000",
+  credentials: true,
+};
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/api/user', UserRouter);
-app.use('/api/project',[Auth], ProjectRouter);
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
+app.use("/api/user", UserRouter);
+app.use("/api/project", [Auth], ProjectRouter);
+app.use("/api", MainRouter);
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
