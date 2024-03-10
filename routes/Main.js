8c2b8c33a6e23257359aa707903bdd7f63856031
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2024-03-10 16:58:18 +0530

    Add route for project-specific endpoints

---
+++ routes/Main.js
@@ -0,0 +1,29 @@
+const Projects = require("../models/Project");
+const Users = require("../models/User");
+const express = require("express");
+const mainRouter = express.Router();
+
+
+const callingFunction = async () => {
+    const allUsers = await Users.find();
+    if (allUsers) {
+      allUsers.forEach(async (user) => {
+        const allProjectAssociatedWithUser = await Projects.find({
+          admin: user._id,
+        });
+        if (allProjectAssociatedWithUser) {
+          allProjectAssociatedWithUser.forEach((project) => {
+            const id = project._id.toHexString();
+            mainRouter.get(`/${project.name}/${id}`, (req, res) => {
+              console.log("Hello World!", project.name);
+              res.status(200).send("working fine " + `${project.name}`);
+            });
+          });
+        }
+      });
+    }
+  };
+
+callingFunction();
+
+  module.exports = mainRouter;
