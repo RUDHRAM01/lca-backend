586130b6a2d441dbad2752ee211751719c1dd958
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2024-03-15 16:45:42 +0530

    Add route to get project by ID

--- routes/ProjectRoute.js
+++ routes/ProjectRoute.js
@@ -4,5 +4,6 @@ const ProjectRouter = require('express').Router();
 
 ProjectRouter.post('/create', ProjectController.createProject);
 ProjectRouter.get('/all', ProjectController.getAllProjects);
+ProjectRouter.get('/get/:id', ProjectController.getProject);
 
 module.exports = ProjectRouter;
