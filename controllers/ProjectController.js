852dbcc0a925e7d3e35ea358a591ced0d4b3c6a6
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2024-03-15 16:45:35 +0530

    Add getProject function to ProjectController.js

--- controllers/ProjectController.js
+++ controllers/ProjectController.js
@@ -40,9 +40,22 @@ const getAllProjects = async (req, res) => {
     }
 }
 
+const getProject = async (req, res) => {
+    try {
+        const project = await Project.findById(req.params.id);
+        if (!project) {
+            return res.status(404).json({ error: "Project not found" });
+        }
+        res.status(200).json({message : project});
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
 
 module.exports = {
     createProject,
     getAllProjects,
+    getProject
 }
 
