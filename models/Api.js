5dfb9f7f35ba2b858e3be3efbce76eb469b45d9f
Rudhram Saraswat <rudhramsaraswat2770@gmail.com>
2024-05-16 15:32:02 +0530

    feat: Add project and schema references to Api model

--- models/Api.js
+++ models/Api.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose');
 
 const ApiSchema = new mongoose.Schema({
+    project: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Project',
+        required: true
+    },
     name: {
         type: String,
+        required: true
+    },
+    schema: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'UserDefine',
         required: true
     },
     endpoint: {
