b98b3ad7c42a6c124c2bfe5a20596bab52112960
Rudhram Saraswat <rudhramsaraswat2770@gmail.com>
2024-05-20 20:48:11 +0530

    chore: Add deleteApi function to ApiController and ApiRoute

--- controllers/ApiController.js
+++ controllers/ApiController.js
@@ -36,8 +36,24 @@ const getApi = async (req, res) => {
     }
 };
 
+const deleteApi = async (req, res) => {
+    try {
+        const find = await Api.findById(req.params.id);
+        const project = await Project.findById(find.project);
+        if (project.admin.toHexString() !== req.user.id) {
+          return res.status(403).json({ error: "Access denied" });
+        }
+        const api = await Api.findByIdAndDelete(req.params.id);
+        if(!api) return res.status(404).json({ error: 'API not found' });
+        mainRouterRoutes();
+        res.status(200).json({ message: 'API deleted successfully' });
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
 
 module.exports = {
     createApi,
-    getApi
+    getApi,
+    deleteApi
 };
--- routes/ApiRoute.js
+++ routes/ApiRoute.js
@@ -5,6 +5,7 @@ const ApiRouter = express.Router();
 
 ApiRouter.post('/create/:id', ApiController.createApi);
 ApiRouter.get('/get/:id', ApiController.getApi);
+ApiRouter.delete('/delete/:id', ApiController.deleteApi);
 
 
 module.exports = ApiRouter;
